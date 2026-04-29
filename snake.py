4d0b5ab09032283c7eaaaafac7c8687810d16b6a
Your Name <maxhamiltonhon@gmail.com>
2026-04-29 16:33:45 -0500

    DONZO

--- snake.py
+++ snake.py
@@ -8,7 +8,7 @@ def playing_area():
     pen = Turtle()
     pen.ht()
     pen.speed(0)
-    pen.color('light blue')
+    pen.color('green')
     pen.begin_fill()
     pen.goto(-240,240)
     pen.goto(240,240)
@@ -18,27 +18,66 @@ def playing_area():
     pen.end_fill()
     
 class Head(Turtle):
-  def __init__(self, screen, body):
+  def __init__(self, screen):
     super().__init__()
-    pass
+    self.ht()
+    self.speed(2)
+    self.color(generate_color())
+    self.player_color = color
+    self.penup()
+    self.setheading(90)
+    self.shape("square")
+    self.direction = "up"
+    self.alive = True
+    self.speed(2)
+    self.st()
+    screen.onkeypress(self.left, "Left")
+    screen.onkeypress(self.right, "Right")
+    screen.onkeypress(self.up, "Up")
+    screen.onkeypress(self.down, "Down")
 
   def up(self):
-    pass
+    if self.direction != "down":
+      self.speed(2)
+      self.setheading(90)
+      self.direction = "up"
+      self.speed(2)
 
   def down(self):
-    pass
+    if self.direction != "up":
+      self.speed(2)
+      self.setheading(-90)  
+      self.direction = "down"
+      self.speed(2)
 
   def left(self):
-    pass
+    if self.direction != "right":
+      self.speed(2)
+      self.setheading(180)
+      self.direction = "left"
+      self.speed(2)
 
   def right(self):
-    pass
+    if self.direction != "left":
+      self.speed(2)
+      self.setheading(0)
+      self.direction = "right"
+      self.speed(2)
 
   def move(self):
-    pass
+    self.forward(5)
+    if self.xcor() < -240:
+      self.die()
+    elif self.xcor() > 240:
+      self.die()
+    elif self.ycor() > 240:
+      self.die()
+    elif self.ycor() < -240:
+      self.die() 
     
   def die(self):
-    pass
+    self.alive = False
+    self.ht()
 
 
 class Segment(Turtle):
@@ -52,24 +91,32 @@ class Segment(Turtle):
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    pass
+    self.ht()
+    self.color("red")
+    self.shape("circle")
+    self.pu()
+    self.goto(random.randint(-230,230),random.randint(-230,230))
+    self.st()
 
   def relocate(self):
-    pass
+    self.ht()
+    self.goto(random.randint(-230,230),random.randint(-230,230))
+    self.st()
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
+playing_area()
 # Key Binding. Connects key presses and mouse clicks with function calls
 screen.listen()
 
 body = []
-
-
-screen.exitonclick()
-
-
-
+head = Head(screen)
+apple = Apple()
+while head.alive == True:
+  head.move()
+  if head.distance(apple) < 20:
+    apple.relocate()
 
 
 
